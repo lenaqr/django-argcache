067ffd0604e98c6b632a041ac1daceeda185f640
Anthony Lu <lua@mit.edu>
2015-11-01 09:47:53 -0500

    Add database settings to tests/settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,16 @@
+import os
+TESTS_DIR = os.path.dirname(__file__)
+
 SECRET_KEY = 'abc'
 
 INSTALLED_APPS = [
     'argcache',
     'tests',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(TESTS_DIR, 'db.sqlite3'),
+    }
+}
