724e40ae856a4b66e07db7ce680067175a82f3fe
Anthony Lu <lua@mit.edu>
2015-11-01 08:25:25 -0500

    Fix copyright header files

--- src/urls.py
+++ src/urls.py
@@ -1,3 +1,27 @@
+
+__author__    = "Individual contributors (see AUTHORS file)"
+__date__      = "$DATE$"
+__rev__       = "$REV$"
+__license__   = "AGPL v.3"
+__copyright__ = """
+This file is part of ArgCache.
+Copyright (c) 2015 by the individual contributors
+  (see AUTHORS file)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from django.conf.urls import *
 
 urlpatterns = patterns('',
