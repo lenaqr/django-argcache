64c0edf1ba9afb9fa9e34bf097d9f41f5605eb52
Anthony Lu <lua@mit.edu>
2015-11-01 08:29:05 -0500

    Add setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-argcache',
+    version='0.1',
+    license='AGPLv3',
+    description='A function-level caching and invalidaton fraemwork for Django.',
+    long_description=README,
+    url='https://github.com/luac/django-argcache/',
+    author='Anthony Lu',
+    author_email='lua@mit.edu',
+    packages=[
+        'argcache'
+    ],
+    package_dir={
+        'argcache': 'src'
+    },
+    include_package_data=True,
+    install_requires=[
+        'django>=1.7',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
